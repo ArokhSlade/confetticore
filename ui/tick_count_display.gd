5dfd3311337501f93e203a58c402605481b9d7f9
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-09-12 21:10:26 +0200

    tick_count_display now counts down, instead of up
    [task #78](https://tree.taiga.io/project/gerald_as-confetticore/task/78)

--- ui/tick_count_display.gd
+++ ui/tick_count_display.gd
@@ -3,4 +3,5 @@ extends Panel
 @onready var tick_count_label = $VBoxContainer/TickCountLabel
 
 func update(tick_count, ticks_per_turn):
-	tick_count_label.text = "Ticks: %d / %d" % [tick_count, ticks_per_turn]
+	var ticks_remaining = ticks_per_turn - tick_count
+	tick_count_label.text = "Ticks: %d / %d" % [ticks_remaining, ticks_per_turn]
