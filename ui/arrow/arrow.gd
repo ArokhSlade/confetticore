3a8fcf78adf54a718e20ef58503d52c7ad221709
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-09-24 19:55:48 +0200

    163 - WIP added arrow scene: tool to be used for ui markers (orders)
    set the from and to parameters (pixel coordinates) and an arrow is drawn.
    
    https://tree.taiga.io/project/gerald_as-confetticore/task/163

---
+++ ui/arrow/arrow.gd
@@ -0,0 +1,27 @@
+@tool
+extends Node2D
+
+@export var from : Vector2i
+@export var to : Vector2i
+
+@export var neck : Line2D
+@export var head : Polygon2D
+
+func _ready():
+	setup()
+
+func setup():
+	update_positions()
+
+func update_positions():
+	neck.points[0] = from as Vector2
+	neck.points[1] = to as Vector2
+	head.position = to as Vector2 
+	var look_direction = (to - from) as Vector2
+	var look_at_target = (head.position) + look_direction
+	look_at_target = self.global_transform * look_at_target
+	head.look_at(look_at_target)
+	# print("point: %s, foot: %s, head: %s, look_direction: %s" % [point.position, foot.position, head.position, look_direction])
+
+func _process(_delta):
+	update_positions()
