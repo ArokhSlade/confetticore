ece289b3f4885e9571f80c4ad0ea946022374364
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-09-29 08:52:57 +0200

    showing/hiding info panel for mech selected/deselected works again in new ui architecture
    (#187
    finish new ui arch implementation)[https://tree.taiga.io/project/gerald_as-confetticore/us/187]

--- ui/hud/mode/planning_mode.gd
+++ ui/hud/mode/planning_mode.gd
@@ -2,6 +2,7 @@ extends HUDMode
 class_name PlanningMode
 
 func update(game_data, ui_data):
+	super(game_data, ui_data)
 	hud.screen_space_hud.execute_button.disabled = false
 	hud.screen_space_hud.tick_count_display.hide()
 	
