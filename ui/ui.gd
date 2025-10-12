8aad231e50b0afec8b834883b8bdc319db5f0133
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-10-12 14:17:31 +0200

    (235) - extracted InputReceiver from PlayerInput, b/c input could potentually be directed at other places.

--- ui/ui.gd
+++ ui/ui.gd
@@ -4,28 +4,36 @@ class_name UI
 signal execute_phase_requested
 
 @onready var hud = $HUD
-
+@onready var input_receiver = $InputReceiver
 
 @export var player_input : PlayerInput
-var data_collector : DataCollector
 
+var data_collector : DataCollector
 const DataCollector = preload("res://game/data_collector.gd")
+
 
 func setup(hex_map, data_collector, player_input : PlayerInput):
 	self.data_collector = data_collector
 	self.player_input = player_input
 	
-	connect_to_player_input_signals()
+	connect_player_to_hud()
+	connect_input_to_player()
 	hud.setup(hex_map)
 
 
-func connect_to_player_input_signals():
+func connect_player_to_hud():
 	for signal_info in player_input.get_signal_list():
 		var handler_name = "_on_player_input_" + signal_info.name
-		var _signal = player_input.get(signal_info.name) as Signal
 		if self.has_method(handler_name):
 			var handler = self.get(handler_name)
-			_signal.connect(handler)
+			player_input.connect(signal_info.name, handler)
+
+func connect_input_to_player():
+	for signal_info in input_receiver.get_signal_list():
+		var handler_name = "_on_input_receiver_"  + signal_info.name
+		if player_input.has_method(handler_name):
+			var handler = player_input.get(handler_name)
+			input_receiver.connect(signal_info.name, handler)
 
 
 func update():
